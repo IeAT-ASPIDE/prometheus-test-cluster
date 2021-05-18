f57092ce4a673541eaa2ac1d21a9d520fe7b45c8
Gabriel Iuhasz <juhasz_gabriel@yahoo.com>
2021-05-18 17:49:41 +0300

    updated readme and master deploy script for openstack experiments

--- README.md
+++ README.md
@@ -21,12 +21,12 @@ This is a vagrant based portable testbed cluster for prometheus
   * sudo ./start-master.sh -h <MASTER_IP>
 * All other VMs
   * sudo ./start-slave.sh spark://<MASTER_IP>:7077
-* To check log go to /usr/local/spark-2.1.1-bin-hadoop2.7/logs/
+* To check log go to /usr/local/spark-3.1.1-bin-hadoop2.7/logs/
 * To run Spark Job
-    * spark-submit --class org.apache.spark.examples.SparkPi --master spark://<MASTER_IP>:7077 /usr/local/spark/examples/jars/spark-examples_2.11-2.1.1.jar 100
+    * spark-submit --class org.apache.spark.examples.SparkPi --master spark://<MASTER_IP>:7077 /usr/local/spark/examples/jars/spark-examples_2.12-3.1.1.jar 100
     * If during execution you get an error related to incompatibilities related to minor Pythin version
     make suer you have the correct PYSPARK_PYTHON env. var.  set.
         * export PYSPARK_PYTHON=/opt/conda/bin/python
 * Submitting Python based applications
-    * ./bin/spark-submit --master spark://<MASTER_IP>:7077 loc/of/file/pySparkPi.py 1000
+    * ./spark-submit --master spark://<master>:7077 ../examples/src/main/python/pi.py 1000
 
--- openstack_scripts/master_script.sh
+++ openstack_scripts/master_script.sh
@@ -12,7 +12,7 @@ cd /opt
 wget https://github.com/prometheus/prometheus/releases/download/v2.13.0-rc.0/prometheus-2.13.0-rc.0.linux-amd64.tar.gz
 tar -xzvf prometheus-2.13.0-rc.0.linux-amd64.tar.gz && cd /opt/prometheus-2.13.0-rc.0.linux-amd64
 
-cat <<EOF > /opt/prometheus-2.13.0-rc.0.linux-amd64/prometheus.yml
+cat  > /opt/prometheus-2.13.0-rc.0.linux-amd64/prometheus.yml <<EOF
 global:
   scrape_interval:     1s # By default, scrape targets every 15 seconds.
   # Attach these labels to any time series or alerts when communicating with
@@ -22,14 +22,34 @@ global:
 # A scrape configuration containing exactly one endpoint to scrape:
 # Here it's Prometheus itself.
 scrape_configs:
-  # The job name is added as a label `job=<job_name>` to any timeseries scraped from this config.
+  # The job name is added as a label  to any timeseries scraped from this config.
   - job_name: 'node-prometheus'
     static_configs:
-      - targets: [10.251.0.184:9100', '10.251.0.142:9100', '10.251.0.78:9100', '10.251.0.176:9100', '10.251.0.63:9100']
+      - targets: ['10.251.0.114:9100', '10.251.0.25:9100', '10.251.0.105:9100', '10.251.0.192:9100']
+  - job_name: 'spark-prometheus-master'
+    metrics_path: /metrics/master/prometheus/
+    static_configs:
+      - targets: ['10.251.0.114:8080']
+#  - job_name: 'spark-prometheus-app'
+#    metrics_path: /metrics/application/prometheus
+#    static_configs:
+#      - targets: ['10.251.0.114:8080']
+  - job_name: 'spark-prometheus-workers'
+    metrics_path: /metrics/prometheus/
+    static_configs:
+      - targets: ['10.251.0.114:8081', '10.251.0.105:8081', '10.251.0.25:8081', '10.251.0.192:8081']
 EOF
 cd /opt/prometheus-2.13.0-rc.0.linux-amd64
 # Start prometheus
-nohup ./prometheus > prometheus.log 2>&1 &
+#nohup ./prometheus > prometheus.log 2>&1 &
+
+# Register node_exporter as service
+sudo mkdir /usr/lib/systemd/system
+sudo cp /home/ubuntu/n_scripts/openstack_services/prometheus.service /usr/lib/systemd/system/
+#sudo cp /vagrant/node_exporter.service /usr/lib/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable prometheus.service
+sudo service prometheus.service start
 
 cd /opt
 
@@ -48,8 +68,8 @@ sudo service grafana-server start
 sudo update-rc.d grafana-server defaults
 
 # Install dependancies
-pip3 install numpy==1.16
-pip3 install scikit-learn==0.21.3
+pip3 install numpy
+pip3 install scikit-learn
 pip3 install tpot
 pip3 install dask[complete]
 pip3 install dask distributed --upgrade
